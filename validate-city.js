a8081c420739dcf0ebe4cb8d931c19c437825516
Erik Christiansen <echristiansen@ufl.edu>
2023-02-08 09:05:06 -0500

    validate city fixes

--- validate-city.js
+++ validate-city.js
@@ -17,9 +17,27 @@ async function validateCity(city) {
 
         // FINE FOR BUYERS AND SELLERS (unless we end up wanting to remove address validation from lead-forms.js)
         req.success(function (result) {
-            let closestCityId = result.closest_city_id;
-            console.log('Successfully found the closest city: ' + closestCityId);
-            toPath = "cities/" + closestCityId;
+            let cityId;
+            console.log(result);
+            console.log('SUPPORT CITY CHECK: ' + result.supportedCity);
+            console.log('SUPPORTED CITY ID: ' + result.cityId);
+            if (result.supportedCity && result.cityId) {
+                cityId = result.cityId;
+                console.log('IS SUPPORTED!');
+                // $("#city-id-storage").attr("value", result.cityId);
+            } else {
+                console.log('NOT SUPPORTED!!!');
+                let closestCityInfo = result.closestSupportedCity;
+                if (closestCityInfo) {
+                    console.log('But do have the closest supported city' + closestCityInfo);
+                    let closestCityId = closestCityInfo.closestCityId;
+                    console.log('Found nearest supported city: ' + closestCityId);
+                    cityId = closestCityId;
+                    // $("#city-id-storage").attr("value", closestCityId);
+                }
+            }
+            toPath = "cities/" + cityId;
+            console.log('Returning this path with cityId: ' + toPath);
         });
 
         req.fail(function () {
