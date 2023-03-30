693bdec20130bc0257f0727990e9bf1a4aaa1cb7
Erik Christiansen <echristiansen@ufl.edu>
2023-03-30 14:49:43 -0400

    added getValidCityId

--- validate-city.js
+++ validate-city.js
@@ -24,7 +24,7 @@ async function validateCity(city) {
                 cityId = result.cityId;
                 toPath = "cities/" + cityId;
                 console.log('IS SUPPORTED!');
-                // $("#city-id-storage").attr("value", result.cityId);
+                // $("#city-id-storage").attr("value", result.cityId); // 03/29/2023 - why was this commented out?
             } else {
                 console.log('NOT SUPPORTED!!!');
                 let closestCityInfo = result.closestSupportedCity;
@@ -33,7 +33,7 @@ async function validateCity(city) {
                     let closestCityId = closestCityInfo.closestCityId;
                     console.log('Found nearest supported city: ' + closestCityId);
                     toPath = "cities/" + closestCityId;
-                    // $("#city-id-storage").attr("value", closestCityId);
+                    // $("#city-id-storage").attr("value", closestCityId); // 03/29/2023 - why was this commented out?
                 } else {
                     console.log("Not a supported city and do not have closest city info");
                     toPath = "city";
@@ -55,4 +55,40 @@ async function validateCity(city) {
     }
     console.log('About to return path: ' + toPath);
     return toPath;
+}
+
+
+async function getValidCityId(cityInput) {
+    console.log('About to validate city' + city);
+    let cityId = "";
+    try {
+        await $.ajax({
+            url: 'https://hhvjdbhqp4.execute-api.us-east-1.amazonaws.com/prod/city',
+            method: 'POST',
+            data: JSON.stringify({ 'city': cityInput }),
+        }).done(function (result) {
+            console.log(result);
+            console.log('SUPPORT CITY CHECK: ' + result.supportedCity);
+            console.log('SUPPORTED CITY ID: ' + result.cityId);
+            if (result.supportedCity && result.cityId) {
+                console.log('IS SUPPORTED!');
+                cityId = result.cityId;
+            } else {
+                console.log('NOT SUPPORTED!!!');
+                let closestCityInfo = result.closestSupportedCity;
+                if (closestCityInfo) {
+                    console.log('But do have the closest supported city' + closestCityInfo);
+                    cityId = closestCityInfo.closestCityId;
+                    console.log('Found nearest supported city: ' + cityId);
+                } else {
+                    console.log("Not a supported city and do not have closest city info");
+                }
+            }
+            $("#city-id-storage").attr("value", cityId); // 03/29/2023 - why was this commented out?
+        });
+    } catch (error) {
+        console.log('(Caught error, not fail): Unable to find closest city' + error);
+    }
+    console.log('About to return path: ' + toPath);
+    return cityId;
 }
