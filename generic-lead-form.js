e45cfff57b49c3967072004077f1742f959f4346
Erik Christiansen <echristiansen@ufl.edu>
2023-04-02 13:55:05 -0400

    address correction additions

--- generic-lead-form.js
+++ generic-lead-form.js
@@ -34,28 +34,32 @@ async function proceedAfterAddressValidated(result) {
     let address = result.addressTextModified;
     console.log('Using this address from the validation response: ' + address);
 
+    continueToForm(address);
+}
+
+function continueToForm(address) {
+    console.log('In continueToForm...');
+    // let address = result.addressTextModified;
+    // let address = $("#address-storage").val();
+    // console.log('Using this address from the validation response: ' + address);
+
     // REQUEST PROPERTY INFO FROM BACKEND
     let agentId = $("#agent-id-storage").val();
     console.log('Proceeding after ADDRESS VALIDATION with agentId ' + agentId);
     pullPropertyInfo(address, agentId); // alternatively, we could do this in the address valdation endpoint
 
-    // $("#seller-form").show();
-    $('#validating-address-loader').removeClass('hide');
-    $('#validating-address-loader').css('display', 'flex');
-    // console.log('Should have just shown seller-form');
-    await delay(1500); // slight delay to let user breathe and pull property info?
-    $("#seller-form").show();
-    $('#validating-address-loader').hide();
-    $('#validating-city-loader').hide();
     $("#selling-home-condition-page").show();
     console.log('Should have just shown selling home condition page');
 
+    $('#validating-address-loader').hide();
+    $('#validating-city-loader').hide();
     $("#zip-code-page").hide();
     $("#condo-unit-page").hide(); // may have never gotten here
     $("#confirm-unit-page").hide(); // may have never gotten here
     $("#enter-different-unit-page").hide(); // may have never gotten here
     $("#invalid-address-page").hide(); // for good measure(?)
 }
+
 
 function getGeneralSessionInfo() {
     let agentId = $("#agent-id-storage").val();
@@ -223,3 +227,187 @@ function handleBounce() {
         $("#finished").attr("value", "no"); // technically "bounced", but just needs any value so we don't re-notify
     }
 }
+
+
+// ADDRESS CORRECTION ELEMENTS
+async function rerouteAfterValidation(result) {
+    console.log('Calling rerouteAfterValidation()...');
+    let sessionId = $("#session-id-storage").val();
+    let visitorType = $("#visitor-type-storage").val();
+    let destination = $("#destination-storage").val(); // agentId
+    let address = $("#address-storage").val(); // house number, street, and unit (if any)
+    let gclid = $("#gclid-storage").val();
+
+    // let cityId = $("#city-id-storage").val();
+    let cityId = result.cityId;
+
+    let toPath;
+    let isSupportedCity = result.supportedCity;
+    console.log('IS SUPPORTED CITY? ' + isSupportedCity);
+    if (typeof isSupportedCity == "undefined" || !isSupportedCity) {
+        let closestCityInfo = result.closestSupportedCity;
+        let closestCityId = closestCityInfo.closestCityId;
+        if (typeof closestCityId != "undefined") {
+            console.log('Got a closest city from BACKEND' + closestCityId);
+            cityId = closestCityId;
+        } else {
+            cityId = "locate";
+        }
+    }
+    reroute(cityId, address, visitorType, destination, sessionId, gclid);
+}
+
+function reroute(cityId, address, visitorType, destination, sessionId, gclid) {
+    let toPath = "cities/" + cityId;
+    let queryString = '?address=' + encodeURIComponent(address) + '&validated=true' + '&destination=' + encodeURIComponent(destination) + '&visitor=' + encodeURIComponent(visitorType) + '&session=' + encodeURIComponent(sessionId);
+    queryString = queryString + '&gclid=' + gclid
+    let routeToPathBase = 'https://agentfixup.com/' + toPath;
+    let routeToUrl = routeToPathBase + queryString;
+
+    let now = new Date();
+    console.log('REROUTE start @' + now.getSeconds() + "." + now.getMilliseconds() + "\n\n");
+    window.location.replace(routeToUrl); // REPLACES the current page, so the back button will take them to homepage rather than here
+}
+
+document.getElementById("no-unit-btn").addEventListener('click', (event) => {
+    console.log('Progressing without re-validating the no-unit address');
+    $("#unit-storage").attr("value", "");
+    $(".unit-display").html("");
+    $("#condo-unit-page").hide(); // ADDED 2-7-2023 - NEED TO REROUTE FROM HERE
+
+    // SHOW LOADER
+    $('#validating-address-loader').removeClass('hide');
+    $('#validating-address-loader').css('display', 'flex');
+
+    // PULL COMPONTENTS FROM STORAGE AND REROUTE REGARDLESS (at this point we should always have a cityId, at least of the closest city)
+    let cityId = $("#city-id-storage").val();
+    let address = $("#address-storage").val();
+    let visitorType = $("#visitor-type-storage").val();
+    let destination = $("#destination-storage").val(); // agentId
+    let sessionId = $("#session-id-storage").val(); // agentId
+    let gclid = $("#gclid-storage").val(); // agentId
+    reroute(cityId, address, visitorType, destination, sessionId, gclid);
+});
+
+document.getElementById("unit-submit-btn").addEventListener('click', (event) => {
+    // STORE UNIT
+    let unit = document.getElementById("unit-input").value.trim();
+    $("#unit-storage").attr("value", unit);
+    $(".unit-display").html(unit);
+    console.log('User entered unit: ' + unit);
+
+    // UPDATE ADDRESS
+    console.log('Adding unit to address : ' + unit);
+    let address = $("#address-storage").val();
+    console.log('Adding to address from storage: ' + address);
+
+    let unitAddress = addUnit(address, unit);
+    console.log('Now validating the address with the unit added: ' + unitAddress);
+
+    // HIDE THE CURRENT PAGE WHILE THE LOADER SHOWS
+    $('#condo-unit-page').hide();
+
+    // SHOW LOADER
+    $('#validating-address-loader').removeClass('hide');
+    $('#validating-address-loader').css('display', 'flex');
+
+    let agentId = $("#destination-storage").val();
+    let sessionId = $("#session-id-storage").val();
+
+    // VALIDATE ADDRESS
+    // correctionAddressValidation(unitAddress, agentId, sessionId, rerouteAfterValidation);
+    correctionAddressValidation(unitAddress, agentId, sessionId, proceedAfterAddressValidated);
+});
+
+document.getElementById("unit-correction-submit-btn").addEventListener('click', (event) => {
+    let unit = document.getElementById("unit-correction-input").value.trim();
+    $("#unit-storage").attr("value", unit);
+    $(".unit-display").html(unit);
+    console.log('User entered unit: ' + unit);
+
+    // UPDATE ADDRESS
+    let street = $("#street-storage").val();
+    let city = $("#city-storage").val();
+    let state = $("#state-storage").val();
+    let zip = $("#zip-storage").val();
+
+    let unitAddress = formUnitAddressString(street, unit, city, state, zip);
+
+    // HIDE THE CURRENT PAGE WHILE THE LOADER SHOWS
+    $('#enter-different-unit-page').hide();
+
+    // SHOW LOADER
+    $('#validating-address-loader').removeClass('hide');
+    $('#validating-address-loader').css('display', 'flex');
+
+    let agentId = $("#destination-storage").val();
+    let sessionId = $("#session-id-storage").val();
+
+    // VALIDATE ADDRESS
+    correctionAddressValidation(unitAddress, agentId, sessionId, proceedAfterAddressValidated);
+});
+
+document.getElementById("unit-is-correct-btn").addEventListener('click', (event) => {
+    // HIDE THE CURRENT PAGE WHILE THE LOADER SHOWS
+    $('#confirm-unit-page').hide();
+
+    // SHOW LOADER
+    $('#validating-address-loader').removeClass('hide');
+    $('#validating-address-loader').css('display', 'flex');
+
+    // PULL COMPONTENTS FROM STORAGE AND REROUTE REGARDLESS (at this point we should always have a cityId, at least of the closest city)
+    let address = $("#address-storage").val();
+    continueToForm(address);
+});
+
+document.getElementById("new-unit-needed-btn").addEventListener('click', (event) => {
+    $("#confirm-unit-page").hide();
+    $("#enter-different-unit-page").show();
+});
+
+
+document.getElementById("zip-submit-btn").addEventListener('click', (event) => {
+    let zipCode = document.getElementById("zip-code-input").value.trim();
+    $("#zip-storage").attr("value", zipCode); // NOTE - NEW ADDED 1/4/2022 (not sure it's necessary)
+
+    let address = $("#address-storage").val();
+    console.log('Adding to address from storage: ' + address);
+
+    let zipCodeAddress = addZipCode(address, zipCode);
+
+    // HIDE THE CURRENT PAGE WHILE THE LOADER SHOWS
+    $('#zip-code-page').hide();
+
+    // SHOW LOADER
+    $('#validating-address-loader').removeClass('hide');
+    $('#validating-address-loader').css('display', 'flex');
+
+    let agentId = $("#destination-storage").val();
+    let sessionId = $("#session-id-storage").val();
+
+    // VALIDATE ADDRESS
+    correctionAddressValidation(zipCodeAddress, agentId, sessionId, proceedAfterAddressValidated);
+    setTimeout(function () { $("#validating-address-loader").hide(); }, 2500);
+});
+
+document.getElementById("submit-address-correction-btn").addEventListener('click', (event) => {
+    event.preventDefault(); // prevent webflow defaults
+    $('#submit-address-correction-btn').val("Going...");
+
+    // HIDE THE CURRENT PAGE WHILE THE LOADER SHOWS
+    $('#invalid-address-page').hide();
+
+    // SHOW LOADER
+    $('#validating-address-loader').removeClass('hide');
+    $('#validating-address-loader').css('display', 'flex');
+
+    let address = document.getElementById("address-correction-input").value.trim();
+    $(".address-display").html(address);
+
+    let agentId = $("#destination-storage").val();
+    let sessionId = $("#session-id-storage").val();
+
+    // RE-VALIDATE ADDRESS
+    correctionAddressValidation(address, agentId, sessionId, proceedAfterAddressValidated); // don't set condo (false), because already did on first pull
+    setTimeout(function () { $("#validating-address-loader").hide(); }, 2000);
+});
